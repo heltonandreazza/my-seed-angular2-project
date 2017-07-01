2a33bf256d2ba3ea64665016b9d4d5a271e07848
Helton Andreazza <helton.prg@gmail.com>
2017-06-30 21:30:56 -0300

    setup index.html and polyfills

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,3 +1,5 @@
+import '../polyfills'; // should be loaded first so the app can use the es6/es7/zonejs features
+
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
 
