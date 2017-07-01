c4cf97ab470a185fe5892c4ad00b419753054693
Helton Andreazza <helton.prg@gmail.com>
2017-06-30 23:29:35 -0300

    setup serve production test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from "@angular/router";
 
 import { HomeComponent } from './home.component';
 
-const appRoutes = Routes = [
+const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'users', loadChildren: './users/users.module#UsersModule' },
 ]
